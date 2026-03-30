05cbadfc60773f079449469931f8dcd4822fa991
Ektadevelops <ektadevelops@gmail.com>
2026-03-30 16:36:26 +0530

    backend first comit

---
+++ kamdhenu-backend/src/data/products.js
@@ -0,0 +1,25 @@
+const products = [
+  {
+    id: 1,
+    name: "A2 Cow Milk",
+    type: "Cow Milk",
+    variant: "A2",
+    price: 80,
+  },
+  {
+    id: 2,
+    name: "A1 Cow Milk",
+    type: "Cow Milk",
+    variant: "A1",
+    price: 60,
+  },
+  {
+    id: 3,
+    name: "A2 Buffalo Milk",
+    type: "Buffalo Milk",
+    variant: "A2",
+    price: 90,
+  },
+];
+
+module.exports = products;
