3dc01086d3c97b3d846aa038073316d1f2a699e5
Ektadevelops <ektadevelops@gmail.com>
2026-04-15 20:42:54 +0530

    mongo db connected

--- kamdhenu-backend/src/controllers/productController.js
+++ kamdhenu-backend/src/controllers/productController.js
@@ -1,7 +1,84 @@
-const products = require("../data/products");
+const Product = require("../models/Product");
 
-const getAllProducts = (req, res) => {
-  res.json(products);
+// GET all products
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
-module.exports = { getAllProducts };
+// GET single product
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// CREATE product
+const createProduct = async (req, res) => {
+  try {
+    const { name, type, variant, price } = req.body;
+
+    const product = await Product.create({
+      name,
+      type,
+      variant,
+      price,
+    });
+
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// UPDATE product
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// DELETE product
+const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.json({ message: "Product deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
